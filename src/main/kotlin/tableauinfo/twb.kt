8a676a44cff6152b5cbc2cb8fa613bb550241a0d
Tim Patterson <tim.wanaka@gmail.com>
2018-06-17 20:28:25 +1200

    Render custom sql

--- src/main/kotlin/tableauinfo/twb.kt
+++ src/main/kotlin/tableauinfo/twb.kt
@@ -5,18 +5,47 @@ import org.w3c.dom.Document
 data class TwbInfo(
         val fileName: String,
         val fileSize: Int,
+        val id: String?,
+        val site: String?,
+        val dataSources: List<DataSource>,
         val thumbnails: List<String>,
         val externalShapes: List<Pair<String, String>>
 )
 
+data class DataSource(
+        val caption: String?,
+        val name: String,
+        val customSqls: List<String>,
+        val tables: List<String>
+)
+
 fun parseTwb(fileName: String, fileSize: Int, xmlDoc: Document): TwbInfo {
 
-    val thumbnails = xmlDoc.xpath("//thumbnails/thumbnail").mapNotNull { it.textContent }
+    val repositoryLocation = xmlDoc.xpath("/workbook/repository-location").firstOrNull()
+    val thumbnails = xmlDoc.xpath("/workbook/thumbnails/thumbnail").mapNotNull { it.textContent }
+    val externalShapes = xmlDoc.xpath("/workbook/external/shapes/shape")
+            .map { it.getAttribute("name")!! to it.textContent!! }
+
+    val dataSources = xmlDoc.xpath("/workbook/datasources/datasource").map { datasource ->
+
+        val customSqls = datasource.xpath(".//relation[@name=\"custom_sql_query\"]").mapNotNull { it.textContent }
+        val tables = datasource.xpath(".//relation[@type=\"table\"]").mapNotNull { it.getAttribute("table") }
+
+        DataSource(
+                caption = datasource.getAttribute("caption"),
+                name = datasource.getAttribute("name")!!,
+                customSqls = customSqls,
+                tables = tables
+        )
+    }
 
     return TwbInfo(
-            fileName,
-            fileSize,
-            thumbnails,
-            listOf()
+            fileName = fileName,
+            fileSize = fileSize,
+            id = repositoryLocation?.getAttribute("id"),
+            site = repositoryLocation?.getAttribute("site"),
+            dataSources = dataSources,
+            thumbnails = thumbnails,
+            externalShapes = externalShapes
     )
 }
--- src/main/kotlin/tableauinfo/view.kt
+++ src/main/kotlin/tableauinfo/view.kt
@@ -6,14 +6,69 @@ import org.w3c.dom.Element
 import kotlin.browser.document
 
 fun renderTwbInfo(twbInfo: TwbInfo): Element {
-    return document.create.div("panel") {
-        p {
-            +"Filename: ${twbInfo.fileName}"
-            +"Filesize: ${twbInfo.fileSize}"
-            twbInfo.thumbnails.forEach {
-                img(src = imageToDataUrl(it))
+    return document.create.div("rounded-border callout grid-x grid-margin-x") {
+        h3("cell") { + twbInfo.fileName }
+        hr("cell")
+
+        h5("cell") { + "General"}
+        table("unstriped kv-table") {
+            tbody {
+                twbInfo.id?.let { tr { td { + "Id:" }; td { + it }} }
+                twbInfo.site?.let { tr { td { + "Site:" }; td { + it }} }
+                tr { td { + "File Size:" }; td { + "${twbInfo.fileSize}" }}
             }
         }
+
+        hr("cell")
+
+        h5("cell") { + "Datasources"}
+        twbInfo.dataSources.forEach { datasource ->
+            div("cell callout") {
+                table("unstriped kv-table") {
+                    tbody {
+                        datasource.caption?.let { tr { td { + "Caption:" }; td { + it }} }
+                        tr { td { + "Name:" }; td { + datasource.name }}
+                    }
+                }
+                if (datasource.tables.isNotEmpty()) {
+                    h6 { + "Tables" }
+                    datasource.tables.forEach {
+                        pre { + it }
+                    }
+                }
+
+                if (datasource.customSqls.isNotEmpty()) {
+                    h6 { + "CustomSql" }
+                    datasource.customSqls.forEach {
+                        pre { + it }
+                    }
+                }
+            }
+        }
+
+        hr("cell")
+
+        h5("cell") { + "Thumbnails"}
+        twbInfo.thumbnails.forEach {
+            div("cell medium-3 small-6") {
+                div("card") {
+                    img(src = imageToDataUrl(it))
+                }
+            }
+        }
+
+        hr("cell")
+
+        h5("cell") { + "External Shapes"}
+        twbInfo.externalShapes.forEach { (name, content) ->
+            div("cell medium-3 small-6") {
+                div("card") {
+                    div("card-divider") { + name }
+                    img(src = imageToDataUrl(content))
+                }
+            }
+        }
+
     }
 }
 
